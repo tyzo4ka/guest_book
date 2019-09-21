55bd304d4d28b69224d2f6518e8f6f11a03823f2
tyzo4ka <tyzo4ka@gmail.com>
2019-09-21 12:33:13 +0600

    add model

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,21 +1,12 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+# from webapp.views import index_view, article_view, article_create_view, article_update_view, article_delete_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path("", index_view, name="index"),
+    # path("article/<int:pk>/", article_view, name="article_view"),
+    # path("article/add/", article_create_view, name="article_add"),
+    # path("article/<int:pk>/edit", article_update_view, name="article_update"),
+    # path('article/<int:pk>/delete', article_delete_view, name="article_delete")
 ]
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано'),
+)
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Имя автора')
+    email = models.EmailField(null=False, blank=False, verbose_name='почта')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    status = models.CharField(max_length=30, null=False, blank=True, default=STATUS_CHOICES[0][0],
+                              choices=STATUS_CHOICES, verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+
+
+
+
+    def __str__(self):
+        return self.text
