54a6f740632eddb1d1b6e8c06df2a514e2f63644
tyzo4ka <tyzo4ka@gmail.com>
2019-09-21 13:47:17 +0600

    add models.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,7 +6,7 @@ STATUS_CHOICES = (
 )
 
 
-class Article(models.Model):
+class Entry(models.Model):
     author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Имя автора')
     email = models.EmailField(null=False, blank=False, verbose_name='почта')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
@@ -14,10 +14,6 @@ class Article(models.Model):
                               choices=STATUS_CHOICES, verbose_name='Статус')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-
-
-
-
 
     def __str__(self):
         return self.text
