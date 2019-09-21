e473171cd14cd56c40c8c7d8d32b750ceb40a3a0
tyzo4ka <tyzo4ka@gmail.com>
2019-09-21 13:48:05 +0600

    change admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Entry
 
-# Register your models here.
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'author', 'email', 'text', 'status', 'created_at', 'updated_at']
+    list_filter = ['author', 'email', 'status']
+    list_display_links = ['pk', 'author']
+    search_fields = ['text']
+    fields = ['author', 'email', 'text', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Entry, EntryAdmin)
