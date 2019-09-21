8ba65ee558e8ec7882882794c1d169c051fe03d1
tyzo4ka <tyzo4ka@gmail.com>
2019-09-21 14:43:29 +0600

    change urls.py

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-# from webapp.views import index_view, article_view, article_create_view, article_update_view, article_delete_view
+from webapp.views import index_view, entry_create_view, entry_delete_view, entry_update_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", index_view, name="index"),
-    # path("article/<int:pk>/", article_view, name="article_view"),
-    # path("article/add/", article_create_view, name="article_add"),
-    # path("article/<int:pk>/edit", article_update_view, name="article_update"),
-    # path('article/<int:pk>/delete', article_delete_view, name="article_delete")
+    path("", index_view, name="index"),
+    path("entry/add/", entry_create_view, name="entry_add"),
+    path("entry/<int:pk>/edit", entry_update_view, name="entry_update"),
+    path('entry/<int:pk>/delete', entry_delete_view, name="entry_delete")
 ]
